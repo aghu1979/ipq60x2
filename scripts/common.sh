5756fc4ca035b5ab4278ef7cad4fbc00214f8008
aghu1979 <aghu@sohu.com>
2025-11-04 14:27:55 +0800

    Create common.sh

---
+++ scripts/common.sh
@@ -0,0 +1,27 @@
+# scripts/common.sh
+#!/bin/bash
+
+# --- 颜色定义 ---
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[1;33m'
+export BLUE='\033[0;34m'
+export BOLD='\033[1m'
+export NC='\033[0m' # 无颜色
+
+# --- 日志记录函数 ---
+log_info() {
+    echo -e "${BLUE}[信息]${NC} $1"
+}
+
+log_success() {
+    echo -e "${GREEN}[成功]${NC} $1"
+}
+
+log_warn() {
+    echo -e "${YELLOW}[警告]${NC} $1"
+}
+
+log_error() {
+    echo -e "${RED}[错误]${NC} $1"
+}
